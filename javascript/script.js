b86a7805f042905598bd7a85d4664446668d18a4
Zane-Gilfillan <zane.gilfillan@gmail.com>
2020-12-26 22:18:06 -0600

    update sale cards with real photos

--- javascript/script.js
+++ javascript/script.js
@@ -2,3 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems, options);
   });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.materialboxed');
+   var instances = M.Materialbox.init(elems, options);
+});
